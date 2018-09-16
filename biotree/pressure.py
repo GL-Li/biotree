37e015be8ab68786ff042ab307ef72d036683604
GL-Li <liguanglai@gmail.com>
2018-09-16 09:09:51 -0400

    v0.1 complete

--- biotree/pressure.py
+++ biotree/pressure.py
@@ -2,9 +2,11 @@
 Process xlsx file of perfusion data.
 """
 
+import os
 import pandas as pd
+import numpy as np
 from matplotlib import pyplot as plt
-from biotree.utilities import model_1567
+from biotree.utilities import model_1567, linear_fit, add_legend
 plt.style.use("ggplot")
 
 
@@ -34,15 +36,48 @@ class Pressure:
         return(self)
 
     def plot_sample(self, sample, base=270, with_model=True, x_shift=0,
-                    color="blue", figsize=(9, 6)):
-        spl = self.data[sample]
-
+                    color="blue", figsize=(8, 5), fit_from=30, fit_to=90):
+        # plot sample
+        spl = self.data[sample].dropna()
+        spl = (spl - base) / 30.7 * 43.6  # to mmHg
         plt.figure(figsize=figsize)
-        plt.plot(spl.index, (spl - base) / 30.7 * 43.6, c=color)
+        plt.plot(spl.index, spl, c=color)
 
+        # plot model
         if with_model:
             mdl = model_1567()
-            plt.scatter(mdl.index + x_shift, mdl, c="red", s=1)
+            plt.scatter(mdl.index + x_shift, mdl, c="red", s=0.5)
+
+        # plot fit line to mercox perfusion
+        mercox = spl.loc[(x_shift + fit_from + 120):(x_shift + fit_to + 120)]
+        fit = linear_fit(mercox)
+        x = np.arange(x_shift + 100, x_shift + 580, 1)
+        y = fit[0] + x * fit[1]
+        plt.plot(x,  y, c="black", lw=1)
+
+        # difference between the flat stage of sample and model
+        flat_median = spl.loc[(x_shift + 90):(x_shift + 106)].median()
+        y_diff = round(flat_median - 41.7, 2)
+
+        plt.annotate(sample, xy=(10, 1110), fontsize=20)
+        plt.annotate("x_shift: " + str(x_shift) + " s", xy=(10, 990),
+                     fontsize=16)
+        plt.annotate("y_diff: " + str(y_diff) + " mmHg", xy=(10, 870),
+                     fontsize=16)
+        plt.annotate("slope: " + str(round(fit[1], 2)) + " mmHg/s",
+                     xy=(10, 750), fontsize=16, color="black")
+
+        plt.xlabel("Time (sec)", fontsize=16)
+        plt.ylabel("Pressure (mmHg)", fontsize=16)
+        plt.ylim(-100, 1200)
+        plt.xlim(-20, spl.index.max() + 20)
+
+        # save x_shift for future use in plot
+        dirname = os.path.dirname(__file__)
+        filename = os.path.join(dirname, 'x_shift.csv')
+        sample_xshift = pd.read_csv(filename, index_col="sample")
+        sample_xshift.loc[sample] = [x_shift, base]
+        sample_xshift.to_csv(filename)
 
     def select(self, cols):
         return(self.data[cols])
@@ -50,10 +85,47 @@ class Pressure:
     def compare_to_1567(self, column):
         pass
 
-    def plot_together(self, sample_xshift_color):
+    def plot_together(self, samples, x_shifts=None, colors=None, alphas=None,
+                      figsize=(8, 5)):
         """
         sample_xshift_color : a list of tuples like
             [("BT1902_HL", 355, "red"), ("BT1903_HL", 467, "blue")]
         """
-        for ele in sample_xshift_color:
-            plt.plot(self.index + ele[1], self.data[ele[0]], c=ele[2])
+        # read sample, x_shift, and base
+        dirname = os.path.dirname(__file__)
+        filename = os.path.join(dirname, 'x_shift.csv')
+        sxb = pd.read_csv(filename, index_col="sample")
+
+        if alphas is None:
+            alphas = [1] * len(samples)
+
+        plt.figure(figsize=figsize)
+
+        if x_shifts is None:
+            if colors is None:
+                for spl, alpha in zip(samples, alphas):
+                    plt.plot(self.index - sxb.loc[spl, "x_shift"],
+                             (self.data[spl] - sxb.loc[spl, "base"]) * 1.42,
+                             alpha=alpha)
+            else:
+                for spl, color, alpha in zip(samples, colors, alphas):
+                    plt.plot(self.index - sxb.loc[spl, "x_shift"],
+                             (self.data[spl] - sxb.loc[spl, "base"]) * 1.42,
+                             c=color, alpha=alpha)
+        else:
+            if colors is None:
+                for spl, x_shift, alpha in zip(samples, x_shifts, alphas):
+                    plt.plot(self.index - x_shift,
+                             (self.data[spl] - sxb.loc[spl, "base"]) * 1.42,
+                             alpha=alpha)
+            else:
+                for spl, x_shift, color, alpha in zip(samples, x_shifts,
+                                                      colors, alphas):
+                    plt.plot(self.index - x_shift,
+                             (self.data[spl] - sxb.loc[spl, "base"]) * 1.42,
+                             c=color, alpha=alpha)
+
+        plt.xlabel("Time (sec)", fontsize=16)
+        plt.ylabel("Pressure (mmHg)", fontsize=16)
+        plt.ylim(-100, 1200)
+        add_legend(loc="upper left")
--- biotree/utilities.py
+++ biotree/utilities.py
@@ -5,6 +5,7 @@ define utility function for use in the package
 import os
 import pandas as pd
 import numpy as np
+from matplotlib import pyplot as plt
 from scipy import stats
 
 
@@ -26,24 +27,24 @@ def read_1567(unit="mmHg"):
 def model_1567():
     # match to BT1567 mannually
     BT1567 = read_1567()
-    sect_0 = BT1567[80:114.4]
+    sect_0 = BT1567[85:110]
     sect_1 = BT1567[116.8:130.4]
     sect_2 = BT1567[136.0:360.0]
 
     fit_1 = linear_fit(sect_1)
     fit_2 = linear_fit(sect_2)
 
-    # section 0 fit
-    x0 = np.arange(40, 136)
-    y0 = sect_0.mean() * np.ones(136 - 40)
+    # section 0 fit, use round() to avoid uncertain floating numbers for index
+    x0 = np.arange(40, 136, 0.1).round(1)
+    y0 = (sect_0.mean() * np.ones(1360 - 400)).round(1)
 
     # section 1 fit
-    x1 = np.arange(100, 140)
-    y1 = fit_1[0] + fit_1[1] * x1
+    x1 = np.arange(100, 140, 0.1).round(1)
+    y1 = (fit_1[0] + fit_1[1] * x1).round(1)
 
     # section 2 fit
-    x2 = np.arange(112, 480)
-    y2 = fit_2[0] + fit_2[1] * x2
+    x2 = np.arange(112, 580, 0.1).round(1)
+    y2 = (fit_2[0] + fit_2[1] * x2).round(1)
 
     x = np.concatenate((x0, x1, x2))
     y = np.concatenate((y0, y1, y2))
@@ -69,3 +70,12 @@ def linear_fit(series):
     )
 
     return((intercept, slope))
+
+
+def add_legend(loc=(1, 0), linewidth=2):
+    """
+    Add legend to a plot with controlled position and linewidth
+    """
+    leg = plt.legend(loc=loc)
+    for legobj in leg.legendHandles:
+        legobj.set_linewidth(linewidth)
